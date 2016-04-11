a55912835710664eef1318e3e4754374bee0fcfc
EmmanuelJucaban <EmmanuelJucaban@gmail.com>
2016-04-10 22:36:57 -0700

    Added mongoose, setup database, and set up post/get routes

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/yelpcamp');
 var app = express();
 
-var campgrounds = [
-    { name: 'Lake Tahoe', image: 'http://hdontap.com/images/uploads/gallery_images/104/riva3.jpeg' },
-    { name: 'Yosemite', image: 'http://castle.eiu.edu/~wow/classes/fa09/Yosemite/yose_valley.jpg' },
-    { name: 'Death Valley', image: 'http://img06.deviantart.net/62bd/i/2012/332/4/6/death_valley__campground_by_alierturk-d5mi6rb.jpg' },
-    { name: 'Lake Tahoe', image: 'http://hdontap.com/images/uploads/gallery_images/104/riva3.jpeg' },
-    { name: 'Yosemite', image: 'http://castle.eiu.edu/~wow/classes/fa09/Yosemite/yose_valley.jpg' },
-    { name: 'Death Valley', image: 'http://img06.deviantart.net/62bd/i/2012/332/4/6/death_valley__campground_by_alierturk-d5mi6rb.jpg' },
-    { name: 'Lake Tahoe', image: 'http://hdontap.com/images/uploads/gallery_images/104/riva3.jpeg' },
-    { name: 'Yosemite', image: 'http://castle.eiu.edu/~wow/classes/fa09/Yosemite/yose_valley.jpg' },
-    { name: 'Death Valley', image: 'http://img06.deviantart.net/62bd/i/2012/332/4/6/death_valley__campground_by_alierturk-d5mi6rb.jpg' }
-
-];
-
-app.use(bodyParser.urlencoded({extended: true}))
+// Set up body-parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Sets the view engine to ejs
 app.set('view engine', 'ejs');
 
-// Home page
+//SChema setup
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+
+
+// Landing page
 app.get('/', function(req, res){
 	res.render('landing');
 });
@@ -29,13 +32,30 @@ app.get('/', function(req, res){
 
 // Campground link
 app.get('/campgrounds', function(req, res){
-	res.render('campgrounds', {campgrounds: campgrounds});
+	Campground.find({}, function(err, campgrounds){
+		if(err){
+			console.log(err);
+		}else {
+			res.render('campgrounds', {campgrounds: campgrounds});
+		}
+	});
 });
 
 
-// Link to add new campground form
+// Show add campground form
 app.get('/campgrounds/new', function(req , res){
 	res.render('new');
+});
+
+// Show single campground
+app.get('campgrounds/:id', function(req, res){
+	Campground.findById({req.body.id}, function(err, campground){
+		if(err){
+			console.log(err);
+		} else {
+
+		}
+	})
 });
 
 
@@ -44,13 +64,19 @@ app.get('/campgrounds/new', function(req , res){
 app.post('/campgrounds', function(req , res){
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name:name, image:image};
-	campgrounds.push(newCampground);
-	res.redirect('campgrounds');
+	var description = req.body.description;
+	var newCampground = {name:name, image:image, description: description};
+	Campground.create(newCampground, function(err, campground){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect('campgrounds');
+		}
+	});
 });
 
 
 // Connect to server
 app.listen(PORT, process.env.IP, function(){
-	console.log('Yelpcamp has started')
+	console.log('Yelpcamp has started');
 });
