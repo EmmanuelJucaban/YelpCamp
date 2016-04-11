46687345383e9c5e3eb8c9016006db36b4c4f5b8
EmmanuelJucaban <EmmanuelJucaban@gmail.com>
2016-04-10 23:22:19 -0700

    Added show 1 campground view and updated add campground form

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ mongoose.connect('mongodb://localhost/yelpcamp');
 var app = express();
 
 // Set up body-parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Sets the view engine to ejs
 app.set('view engine', 'ejs');
 
 //SChema setup
 var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
+    name: String,
+    image: String,
+    description: String
 });
 
 
@@ -25,58 +25,60 @@ var Campground = mongoose.model('Campground', campgroundSchema);
 
 
 // Landing page
-app.get('/', function(req, res){
-	res.render('landing');
+app.get('/', function(req, res) {
+    res.render('landing');
 });
 
 
 // Campground link
-app.get('/campgrounds', function(req, res){
-	Campground.find({}, function(err, campgrounds){
-		if(err){
-			console.log(err);
-		}else {
-			res.render('campgrounds', {campgrounds: campgrounds});
-		}
-	});
+app.get('/campgrounds', function(req, res) {
+    Campground.find({}, function(err, campgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', { campgrounds: campgrounds });
+        }
+    });
 });
+
 
 
 // Show add campground form
-app.get('/campgrounds/new', function(req , res){
-	res.render('new');
-});
-
-// Show single campground
-app.get('campgrounds/:id', function(req, res){
-	Campground.findById({req.body.id}, function(err, campground){
-		if(err){
-			console.log(err);
-		} else {
-
-		}
-	})
+app.get('/campgrounds/new', function(req, res) {
+    res.render('new');
 });
 
 
 
 // To post a campground
-app.post('/campgrounds', function(req , res){
-	var name = req.body.name;
-	var image = req.body.image;
-	var description = req.body.description;
-	var newCampground = {name:name, image:image, description: description};
-	Campground.create(newCampground, function(err, campground){
-		if(err){
-			console.log(err);
-		} else {
-			res.redirect('campgrounds');
-		}
-	});
+app.post('/campgrounds', function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var newCampground = { name: name, image: image, description: description };
+    Campground.create(newCampground, function(err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('campgrounds');
+        }
+    });
 });
 
 
+
+// Shows more info about one campground
+app.get('/campgrounds/:id', function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+    	if(err){
+    		console.log(err);
+    	} else {
+    		res.render('show', {campground: foundCampground});
+    	}
+    });
+});
+
 // Connect to server
-app.listen(PORT, process.env.IP, function(){
-	console.log('Yelpcamp has started');
+app.listen(PORT, process.env.IP, function() {
+    console.log('Yelpcamp has started');
 });
