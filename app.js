2b35fb01e13a5fa3c867e0c262d92b1b3339be14
Emmanuel <emmanueljucaban@gmail.com>
2016-04-12 21:39:26 -0700

    Added Post routes to comment form and added add comment button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Campground = require('./models/campground');
+var Comment = require('./models/comment');
 var seedDB = require('./seed');
 
 
@@ -90,7 +91,24 @@ app.get('/campgrounds/:id/comments/new', function(req, res) {
     });
 });
 
-
+app.post('/campgrounds/:id/comments', function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect('/campgrounds');
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
+        }
+    });
+})
 
 // Connect to server
 app.listen(PORT, process.env.IP, function() {
