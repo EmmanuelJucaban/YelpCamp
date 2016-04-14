75ca7626b8e690280742708d48e2cbec5839abbd
EmmanuelJucaban <EmmanuelJucaban@gmail.com>
2016-04-14 03:10:42 -0700

    Added Destroy functionality to Campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -89,4 +89,18 @@ router.put('/:id', function(req, res){
         }
     })
 });
+
+
+
+// DESTROY CAMPGROUND ROUTE
+router.delete('/:id', function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds');
+        }
+    });
+});
+
 module.exports = router;
