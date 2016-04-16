65a5d2badd3b8da4542644e45206618967683fc1
Emmanuel <emmanueljucaban@gmail.com>
2016-04-15 21:51:58 -0700

    Centered text on landing page

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-var PORT        = process.env.PORT || 3000,
-    express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    flash       = require("connect-flash"),
-    passport    = require("passport"),
+var PORT = process.env.PORT || 3000,
+    express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seed")
-    
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    User = require("./models/user"),
+    seedDB = require("./seed")
+
 //requiring routes
-var commentRoutes    = require("./routes/comments"),
+var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index")
-    
+    indexRoutes = require("./routes/index")
 
-mongoose.connect(process.env.DATABASEURL);
+if (PORT === 3000) {
+    mongoose.connect("mongodb://localhost/yelpcamp");
+} else {
+    mongoose.connect(process.env.DATABASEURL);
+}
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -40,11 +43,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   next();
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
 });
 
 app.use("/", indexRoutes);
@@ -52,6 +55,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+app.listen(PORT, process.env.IP, function() {
+    console.log("The YelpCamp Server Has Started!");
 });
